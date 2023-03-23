a389387128f8d12721e255a8b56504932ea665b7
SUNIL DHARAWAT <90829652+SunilDharawat@users.noreply.github.com>
2023-03-23 12:59:14 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+package com.patterns.program;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+public static void main(String[] args) {
+	int n,r;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	n=sc.nextInt();
+	for (r = 1;  r<=n; r++) {
+		System.out.print("* ");
+		
+	}
+}
+}
